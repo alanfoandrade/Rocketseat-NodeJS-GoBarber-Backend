f7a7e879ad38efc5155d7d66fbc1cb08c623c7b5
alanfoandrade <alanfrank@gec.inatel.br>
2019-10-21 00:29:05 -0300

    Add File Uploads

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/authMiddleware';
+import multerConfig from './config/multerConfig';
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -11,5 +15,7 @@ routes.get('/users/:userId?', UserController.list);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
