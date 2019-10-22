286264dbf0c9203a4614e8909a444dace880cdf8
alanfoandrade <alanfrank@gec.inatel.br>
2019-10-22 17:35:41 -0300

    Add Sentry e exceptionHandler

--- src/app.js
+++ src/app.js
@@ -1,17 +1,25 @@
 import express from 'express';
 import path from 'path';
+import * as Sentry from '@sentry/node';
+import Youch from 'youch';
+import 'express-async-errors';
 import routes from './routes';
+import sentryConfig from './config/sentryConfig';
 import './database';
 
 class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
 
     // Servir arquivos estaticos (imagem, css, html) para serem acessados diretamente pelo navegador
@@ -23,6 +31,15 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
---
+++ src/config/sentryConfig.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://a110edaadca64c39b9cb03f3f99a484d@sentry.io/1792921'
+};
