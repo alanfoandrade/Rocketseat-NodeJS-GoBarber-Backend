e77a0515448e96fa06fd772e5b97e2f61ba0df30
alanfoandrade <alanfrank@gec.inatel.br>
2019-10-22 18:00:06 -0300

    Add variaveis de ambiente

--- src/config/authConfig.js
+++ src/config/authConfig.js
@@ -1,4 +1,4 @@
 export default {
-  secret: '90e5bb77-4354-4c45-b902-305d12e79d45',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d'
 };
--- src/config/mailConfig.js
+++ src/config/mailConfig.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '4e5343e98d48b2',
-    pass: '519a71d86ece43'
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>'
