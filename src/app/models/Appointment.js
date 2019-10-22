e07cc213cebda03a674181b66615fa549a2237e0
alanfoandrade <alanfrank@gec.inatel.br>
2019-10-22 17:09:19 -0300

    Add propriedades past e cancelable em Appointment

--- src/app/models/Appointment.js
+++ src/app/models/Appointment.js
@@ -1,11 +1,27 @@
 import Sequelize, { Model } from 'sequelize';
+import { isBefore, subHours } from 'date-fns';
 
 class Appointment extends Model {
   static init(sequelize) {
     super.init(
       {
         date: Sequelize.DATE,
-        canceled_at: Sequelize.DATE
+        canceled_at: Sequelize.DATE,
+        past: {
+          // Propriedade virtual não persiste na tabela
+          type: Sequelize.VIRTUAL,
+          get() {
+            // retorna true caso horário já tenha passado
+            return isBefore(this.date, new Date());
+          }
+        },
+        cancelable: {
+          type: Sequelize.VIRTUAL,
+          // verifica se o horário atual é no mínimo 2 horas antes do agendamento
+          get() {
+            return isBefore(new Date(), subHours(this.date, 2));
+          }
+        }
       },
       { sequelize }
     );
